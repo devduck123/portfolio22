57efbeaa6b4255129682f328beb779ea142f8936
tommy <tommyho@cpp.edu>
2022-08-25 14:56:04 -0700

    added favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,6 +9,24 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="w-screen flex flex-col min-h-screen items-center py-2 mx-4 mt-10">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="description" content="Software Engineer Portfolio" />
         <meta name="og:title" content={siteTitle} />
       </Head>
